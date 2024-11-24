69f274d1fc8b727771fe961eee74e7797cd0a4ae
Zhilina Elena <elenazhilina@yandex.ru>
2024-11-24 23:29:37 +0500

    Espresso

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTest.java
@@ -0,0 +1,35 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testTextOnHomeFragment() {
+        ViewInteraction mainText = onView(
+                withId(R.id.text_home) // Используем ID text_home
+        );
+
+        mainText.check(
+                matches(
+                        withText("This is home fragment") // Проверяемый текст
+                )
+        );
+    }
+}
